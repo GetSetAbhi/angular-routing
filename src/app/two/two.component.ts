d15131d2d44d1cd2c64748095466738ef582b237
GetSetAbhi <araina97@gmail.com>
2022-07-24 16:23:33 +0530

    3 files modified

--- src/app/two/two.component.ts
+++ src/app/two/two.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-two',
   templateUrl: './two.component.html',
-  styleUrls: ['./two.component.css']
+  styleUrls: ['./two.component.css'],
 })
 export class TwoComponent implements OnInit {
+  constructor(private activatedRoute: ActivatedRoute) {}
 
-  constructor() { }
+  name: string;
 
   ngOnInit() {
+    this.activatedRoute.queryParams.subscribe((params) => {
+      const userId = params['firstName'] + params['lastName'];
+      if (userId) {
+        this.name = userId;
+        console.log(userId);
+      }
+    });
   }
-
 }
